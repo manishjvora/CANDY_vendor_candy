ae3c785eac9ff56f3cdeeef472719bd21e201258
Raleigh Matlock <raleighman2@gmail.com>
2016-08-28 10:01:42 -0400

    Use a one-size-fits-all boot animation for now
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -5,8 +5,3 @@ $(call inherit-product, vendor/turbo/config/common_full.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Helium.ogg
-
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/turbo/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif
