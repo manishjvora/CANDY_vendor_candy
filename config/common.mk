nychitman1/android_vendor_du

Set new pixel sounds as defaults
Change-Id: I9f60a982e2d103fdb5b0e414d82099288dce76f7

config/common.mk
@@ -37,3 +37,6 @@ include vendor/du/config/packages.mk
 
 # Branding
 include vendor/du/config/branding.mk
+
+# Google sounds
+include vendor/du/google/GoogleAudio.mk

manishjvora/CANDY_vendor_candy

config/common.mk
@@ -208,3 +208,6 @@ ifeq ($(HOST_OS),linux)
         include vendor/candy/sdclang/sdclang.mk
     endif
 endif
+
+# Google sounds
+include vendor/candy/google/GoogleAudio.mk