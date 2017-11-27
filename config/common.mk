DirtyUnicorns/android_vendor_du

Fix Google dialer
Add in dialer_experience.xml found in the factory image so if the user
decides to install it, they're not meant with a message about how is not
supported.

Change-Id: I372ff394e3704dd6380812140a4e35cfbfb8f852

config/common.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc

manishjvora/CANDY_vendor_candy

config/common.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/etc/mkshrc:system/etc/mkshrc \