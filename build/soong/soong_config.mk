dc56baff0e8528c532c06b55431fbf209d8b4a6e
Dan Cartier <nospamdan1@gmail.com>
2017-12-15 04:57:08 +0200

    Revert "candy: Export forced shims list to soong"
    This reverts commit 176963cec935e14b48721b2769d8acd59e1b91c4.
    
    Change-Id: Iab8cf258a54bd7de22aa0b4661c368d9f8c3e7d0

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,7 +5,6 @@ candy_soong:
         echo '"Candy": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Linker_forced_shim_libs": "$(LINKER_FORCED_SHIM_LIBS)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
         echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
